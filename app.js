fc17092973e6d694ec79fe795d6a6c39c1d8181e
maiia-kuzmishyna <113444284+maiia-kuzmishyna@users.noreply.github.com>
2023-04-26 14:22:02 +0200

    Update app.js in pull requests

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const feature_1 = () => {
     console.log("Hey, I am feature 1");
 }
 
+const feature_pull_requests = () => {
+    console.log("Hey, I am feature pull requests");
+}
+
 const feature_2 = () => {
     console.log("Hey, I am feature 2");
 }
