4074ac53366e5b8f3e3c3be23e0f9a9e9e0ccf83
maiia-kuzmishyna <113444284+maiia-kuzmishyna@users.noreply.github.com>
2023-04-26 14:21:20 +0200

    Feature 1.5

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const feature_1 = () => {
     console.log("Hey, I am feature 1");
 }
 
+const feature_1_5 = () => {
+    console.log("Hey, I am feature 2");
+}
+
 const feature_2 = () => {
     console.log("Hey, I am feature 2");
 }
